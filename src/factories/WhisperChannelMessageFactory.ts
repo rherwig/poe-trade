d450a444dc174daeb606ab2dc69dd182ec3b0cd5
Rico Herwig <rherwig4711@gmail.com>
2020-04-12 21:38:45 +0200

    Fix control message regex

--- src/factories/WhisperChannelMessageFactory.ts
+++ src/factories/WhisperChannelMessageFactory.ts
@@ -3,12 +3,17 @@ import { Service } from 'typedi';
 import { IFactory } from '../interfaces/IFactory';
 import WhisperChannelMessage from '../models/messages/WhisperChannelMessage';
 import IncomingWhisperMessage from '../models/messages/IncomingWhisperMessage';
+import OutgoingWhisperMessage from '../models/messages/OutgoingWhisperMessage';
 
 @Service('WhisperChannelMessageFactory')
 export default class WhisperChannelMessageFactory implements IFactory<WhisperChannelMessage> {
 
     public create(payload: string): WhisperChannelMessage {
-        return new IncomingWhisperMessage('');
+        if (payload.startsWith('@From')) {
+            return new IncomingWhisperMessage(payload);
+        }
+
+        return new OutgoingWhisperMessage(payload);
     }
 
     public supports(payload: string): boolean {
--- src/models/messages/ControlMessage.ts
+++ src/models/messages/ControlMessage.ts
@@ -3,7 +3,7 @@ import AbstractMessage from './AbstractMessage';
 export default class ControlMessage extends AbstractMessage {
     constructor(payload: string) {
         const prefix = ':';
-        const payloadRegex = new RegExp(`${prefix}\\s?()(.*)`);
+        const payloadRegex = new RegExp(`(?:${prefix}\\s?)(.*)`);
 
         super(payload, {
             prefix,
